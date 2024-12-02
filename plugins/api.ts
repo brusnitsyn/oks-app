9bae0115433f7ab1da2bce902338abed4df17625
brusnitsyn <brusnitsyn@github.com>
2024-12-02 20:13:13 +0900

    u

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,29 +1,30 @@
 import { useRequestURL } from 'nuxt/app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const url = useRequestURL()
-  const ip = url.hostname
-  let baseURL = 'http://46.16.9.130:49022/'
-  switch (ip) {
-    case '127.0.0.1':
-      baseURL = 'http://46.16.9.130:49022/'
-      break
-    case '10.32.0.204':
-      baseURL = 'http://10.32.0.204:82/'
-      break
-    case '46.16.9.130':
-      baseURL = 'http://46.16.9.130:49022/'
-      break
-  }
   const api = $fetch.create({
-    baseURL,
     onRequest({ request, options, error }) {
-      console.log(options.baseURL)
-      console.log(request)
+      const url = useRequestURL()
+
+      const ip = url.hostname
+      let baseURL = 'http://46.16.9.130:49022/'
+      switch (ip) {
+        case '127.0.0.1':
+          baseURL = 'http://46.16.9.130:49022/'
+          break
+        case '10.32.0.204':
+          baseURL = 'http://10.32.0.204:82/'
+          break
+        case '46.16.9.130':
+          baseURL = 'http://46.16.9.130:49022/'
+          break
+      }
+
+      options.baseURL = baseURL
+
       const token = useCookie('token')
       const accessToken = token.value
-      console.log(accessToken)
-      if (useCookie('token').value) {
+      // console.log(accessToken)
+      if (accessToken) {
         options.mode = options.mode ?? 'cors'
         const headers = options.headers ||= {}
         if (Array.isArray(headers)) {
