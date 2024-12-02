464c9ef0a29b2ffc8592ca1681fc0bc9163d1a7f
brusnitsyn <andrew.brusnitsyn@gmail.com>
2024-12-02 10:37:13 +0900

    u

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,8 +1,24 @@
+import { useRequestURL } from 'nuxt/app'
+
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
   const cookieToken = useCookie('token')
+  const url = useRequestURL().host
+  let baseURL = 'http://127.0.0.1:8000/api'
+  console.log(url)
+  switch (url) {
+    case '127.0.0.1:3000':
+      baseURL = 'http://127.0.0.1:8000/'
+      break
+    case '10.32.0.204:31':
+      baseURL = 'http://10.32.0.204:32/'
+      break
+    case '46.16.9.130:49021':
+      baseURL = 'http://46.16.9.130:49022/'
+      break
+  }
   const api = $fetch.create({
-    baseURL: config.public.apiUrl ?? 'http://127.0.0.1:8000/api',
+    baseURL,
     onRequest({ request, options, error }) {
       if (cookieToken.value) {
         options.mode = options.mode ?? 'cors'
